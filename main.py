f643fb6151f5ca5e1c0c20b88a35c76370f2a71d
Anshul Jain <jain.anshul1304@gmail.com>
2023-12-10 15:13:56 +0530

    Project Complete

--- main.py
+++ main.py
@@ -1,9 +1,49 @@
-from flask import Flask,render_template
+from flask import Flask, render_template, jsonify, request
+from flask_pymongo import PyMongo
+import openai
+
+openai.api_key = "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"
+
+
+
+
 app = Flask(__name__)
+app.config["MONGO_URI"] = "mongodb+srv://jainanshul1304:77wonders@promptmaster.f0en01l.mongodb.net/ChatGPT"
+mongo = PyMongo(app)
 
-@app.route('/')
-def hello_world():
-    return render_template("index.html")
+@app.route("/")
+def home():
+    chats = mongo.db.chats.find({})
+    myChats = [chat for chat in chats]
+    print(myChats)
+    return render_template("index.html", myChats = myChats)
 
-app.run(debug=True)
+@app.route("/api", methods=["GET", "POST"])
+def qa():
+    if request.method == "POST":
+        print(request.json)
+        question = request.json.get("question")
+        chat = mongo.db.chats.find_one({"question": question})
+        print(chat)
+        if chat:
+            data = {"question": question, "answer": f"{chat['answer']}"}
+            return jsonify(data)
+        else:
+            response = openai.completions.create(
+                    model="text-davinci-003",
+                    prompt=question,
+                    temperature=0.7,
+                    max_tokens=256,
+                    top_p=1,
+                    frequency_penalty=0,
+                    presence_penalty=0
+                    )
+            print(response)
+            data = {"question": question, "answer": response.choices[0].text}
+            mongo.db.chats.insert_one({"question": question, "answer": response.choices[0].text})
+            return jsonify(data)
+    data = {"result": "Thank you! I'm just a machine learning model designed to respond to questions and generate text based on my training data. Is there anything specific you'd like to ask or discuss? "}
+        
+    return jsonify(data)
 
+app.run(debug=True, port=5001)
